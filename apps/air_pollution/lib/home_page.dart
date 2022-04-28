d7da5de6b55f5236cae90f08fa68812223ecc137
uni <yuni.todate@gmail.com>
2022-04-28 14:15:03 +0000

    load tdata o home ppage

--- apps/air_pollution/lib/home_page.dart
+++ apps/air_pollution/lib/home_page.dart
@@ -1,3 +1,5 @@
+import 'package:air_pollution/openmap.dart';
+import 'package:air_pollution/openmap_api.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -9,16 +11,28 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   String _key = 'api key';
+  late Future<AirPollution> futureAirPollution;
 
   @override
+  void initState() {
+    super.initState();
+    // requet once
+    futureAirPollution = fetchAirPollution();
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(title: Text('poll')),
         body: Container(
-          child: FutureBuilder<Map<String, dynamic>>(
+          child: FutureBuilder<AirPollution>(
+              future: futureAirPollution,
               builder: (BuildContext context, AsyncSnapshot snapshot) {
-            return Text("main page");
-          }),
+                if (snapshot.hasData) {
+                  return Text(snapshot.data!.toString());
+                } else if (snapshot.hasError) {
+                  return Text('${snapshot.error}');
+                }
+              }),
         ));
   }
 }
